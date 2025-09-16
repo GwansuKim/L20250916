68d7a49ff88643a4569b310885a869e8187469e7
GwansuKim <121069568+GwansuKim@users.noreply.github.com>
2025-09-16 10:41:19 +0900

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int MyMoney = 1000000;
+	int Pay = 2000000;
+	int Tax = 10;
+
+	int RealPay = Pay * (100 - Tax) / 10;
+
+	MyMoney += RealPay;
+
+	cout << MyMoney;
+
+	return 0;
+}
